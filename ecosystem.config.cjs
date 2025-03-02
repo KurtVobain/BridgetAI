d8dc920b6bd0f56412d2f9e717d9c76da77cc45f
Evgenii Karpov <one1two215@yandex.ru>
2025-03-02 18:58:44 +0100

    Changed Eliza version

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [
       {
-        name: "chump-agent",
+        name: "bridget-agent",
         script: "pnpm",
-        args: "start --character=./characters/chump.character.json",
+        args: "start --character=./characters/bridget.character.json",
         cron_restart: "0 */12 * * *",
         watch: false,
         autorestart: true,
