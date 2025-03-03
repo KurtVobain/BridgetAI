5adecf77c071606f74d001ad7843d3f6bd567173
Evgenii Karpov <one1two215@yandex.ru>
2025-03-03 11:02:07 +0100

    Updated ecosystem.config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
         script: "pnpm",
         args: "start --character=./characters/bridget.character.json",
         cron_restart: "0 */12 * * *",
+        cwd: "/root/bridget-agent-apis",
         watch: false,
         autorestart: true,
       },
